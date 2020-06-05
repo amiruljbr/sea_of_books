8add812eafb4d52e5436eb7b228c24ac802caf99
amiruljbr <amiruljabar@gmail.com>
2020-06-05 10:23:26 +0700

    done

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,21 @@ const routes = require('express').Router();
 const routesBook = require('./book');
 const routesReview = require('./review');
 const routesUser = require('./user');
+// let session = ((req,res,next) =>{
+//   if ((req.session.userId)) {
+//     next()
+//   }else{
+//     res.send("Harus login terlebih dahulu")
+//   }
+// })
 
 routes.get('/',(req,res)=>{
-  res.render('home')
+  res.render('home2')
 });
+routes.use('/users',routesUser);
+// routes.use(session);
 routes.use('/books',routesBook);
 routes.use('/reviews',routesReview);
-routes.use('/users',routesUser);
+
 
 module.exports = routes;
